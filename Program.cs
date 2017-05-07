79743eab6eccbda5e07acffd8716563a9e022f5c
Philip Doxakis <doxakisp@gmail.com>
2017-05-07 00:36:15 -0400

    feat: proof of concept working

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PythonDotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listIndex = new List<string>();
+            for (int i = 1; i < 26; i++)
+            {
+                listIndex.Add(i + "");
+            }
+
+            Parallel.ForEach(listIndex, new ParallelOptions { MaxDegreeOfParallelism = 4 }, (index) =>
+            {
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.FileName = "python";
+                p.StartInfo.Arguments = "my_python_demo.py " + index + " " + 10;
+                p.StartInfo.RedirectStandardOutput = true;
+
+                p.Start();
+
+                p.WaitForExit();
+
+                var result = p.StandardOutput.ReadToEnd();
+                Console.WriteLine("Result for #" + index + " : " + result.Trim());
+            });
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Execute python code from dotnet core
+This is a proof of concept.
+
+I ran it on Mac OS.
+
+## Key points:
+
+- Run c# on Mac OS X
+- Parallel execution with a degree of parallelism
+- Send parameters to python script from c#
+- Redirect the output to the console
+
+# Copyright and license
+Code released under the MIT license.
